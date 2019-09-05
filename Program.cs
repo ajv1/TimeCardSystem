6072f803be9532d23adc23430b41e5de88c0d1db
Adam Viera <adamjviera@gmail.com>
2019-09-05 16:47:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TimeCardSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How many days of leave would you like to take off?: ");
+                int days = int.Parse(Console.ReadLine());
+
+                if (days < 7 &&  days > 1)
+                {
+                    Console.WriteLine("You can take off!");
+                }
+                else Console.WriteLine("You aren't eligible for the days requested.");
+                }
+            catch
+            {
+                Console.WriteLine("You have an error!");
+            }
+        }
+    }
+}
